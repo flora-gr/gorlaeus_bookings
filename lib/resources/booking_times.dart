26dba7390d8050a0d85c18c47559750c7d57ae7a
Flora Groothuizen <floragroothuizen@gmail.com>
2022-09-07 17:40:05 +0200

    Remove first time block

--- lib/resources/booking_times.dart
+++ lib/resources/booking_times.dart
@@ -5,42 +5,38 @@ class BookingTimes {
   const BookingTimes._();
 
   static const TimeBlock time1 = TimeBlock(
-    startTime: TimeOfDay(hour: 8, minute: 0),
-    endTime: TimeOfDay(hour: 9, minute: 0),
-  );
-  static const TimeBlock time2 = TimeBlock(
     startTime: TimeOfDay(hour: 9, minute: 0),
     endTime: TimeOfDay(hour: 10, minute: 0),
   );
-  static const TimeBlock time3 = TimeBlock(
+  static const TimeBlock time2 = TimeBlock(
     startTime: TimeOfDay(hour: 10, minute: 0),
     endTime: TimeOfDay(hour: 11, minute: 0),
   );
-  static const TimeBlock time4 = TimeBlock(
+  static const TimeBlock time3 = TimeBlock(
     startTime: TimeOfDay(hour: 11, minute: 0),
     endTime: TimeOfDay(hour: 12, minute: 0),
   );
-  static const TimeBlock time5 = TimeBlock(
+  static const TimeBlock time4 = TimeBlock(
     startTime: TimeOfDay(hour: 12, minute: 0),
     endTime: TimeOfDay(hour: 13, minute: 0),
   );
-  static const TimeBlock time6 = TimeBlock(
+  static const TimeBlock time5 = TimeBlock(
     startTime: TimeOfDay(hour: 13, minute: 0),
     endTime: TimeOfDay(hour: 14, minute: 0),
   );
-  static const TimeBlock time7 = TimeBlock(
+  static const TimeBlock time6 = TimeBlock(
     startTime: TimeOfDay(hour: 14, minute: 0),
     endTime: TimeOfDay(hour: 15, minute: 0),
   );
-  static const TimeBlock time8 = TimeBlock(
+  static const TimeBlock time7 = TimeBlock(
     startTime: TimeOfDay(hour: 15, minute: 0),
     endTime: TimeOfDay(hour: 16, minute: 0),
   );
-  static const TimeBlock time9 = TimeBlock(
+  static const TimeBlock time8 = TimeBlock(
     startTime: TimeOfDay(hour: 16, minute: 0),
     endTime: TimeOfDay(hour: 17, minute: 0),
   );
-  static const TimeBlock time10 = TimeBlock(
+  static const TimeBlock time9 = TimeBlock(
     startTime: TimeOfDay(hour: 17, minute: 0),
     endTime: TimeOfDay(hour: 18, minute: 0),
   );
@@ -55,6 +51,5 @@ class BookingTimes {
     time7,
     time8,
     time9,
-    time10,
   ];
 }
